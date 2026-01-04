d7e013143183eceeed4c80e2d5f321ff54f80ba0
Haseetha-03 <ahaladhinitummala@gmail.com>
2026-01-04 18:05:13 +0530

    server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const app=express()
+const app=express();
+const FirstRoute=require("./src/Routes/FirstRoutes")
+app.use("/",FirstRoute);
 app.listen(6000,()=>{
-    console.log("Server start at port 9000")
+    console.log("Server start at port 6000");
 })
